d5d17e806b4c476f17e47af918bd553e85df6ebc
Peng Xu <pengxu@Pengs-MacBook-Pro.local>
2017-12-05 21:35:03 -0500

    tools done

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,36 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  if(estimations.size() == 0){
+    cout << "ERROR - CalculateRMSE () - The estimations vector is empty" << endl;
+    return rmse;
+  }
+
+  if(ground_truth.size() == 0){
+    cout << "ERROR - CalculateRMSE () - The ground-truth vector is empty" << endl;
+    return rmse;
+  }
+
+  if(estimations.size() != ground_truth.size()){
+    cout << "ERROR - CalculateRMSE () - The ground-truth and estimations vectors must have the same size." << endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  for(int i = 0; i < estimations.size(); ++i){
+	VectorXd residual = estimations[i] - ground_truth[i];
+	residual = residual.array() * residual.array();
+	rmse += residual;
+  }
+
+  //calculate the mean
+  rmse /= estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
